c78866c28c997681ae3e6f61ecd90a8b381ddcff
Asilbek <165985168+asilbekali@users.noreply.github.com>
2024-08-29 12:46:00 +0500

    GM uchun keraklik bolgan suratlar mavjud
    qanday qilib 1 oynada 2 oynaga o'tishlik haqida kod bor

---
+++ GM/test2.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import *
+
+class Oyna2(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(500, 500)  # Example fixed size
+        self.setWindowTitle("Oyna2 Window")  # Example title
+
